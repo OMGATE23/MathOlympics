8a2c71a886cfec1e479e2e79c66d6f841b44f121
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-07-30 19:13:40 +0530

    feat : exporting every hook

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export {useGetMathEquation} from './useGetMathEquation.ts'
+export {useGetEquationParams} from './useGetEquationParams.ts'
+export {getEncouragement} from './getEncouragement.ts'
+export * from './localStorageHandler.ts'
